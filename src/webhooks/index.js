b02383d000735e8b6dadfe887a2524de94c48942
JP <jpsala@gmail.com>
2020-06-12 22:52:07 -0300

    está grabando los hooks en assets/webhook

--- src/webhooks/index.js
+++ src/webhooks/index.js
@@ -1,16 +1,20 @@
+/* eslint-disable camelcase */
 import express from 'express';
 import fs from 'fs';
 import { join } from 'path';
 import moment from 'moment';
 import 'moment/locale/es-us';
 
+const { spawn } = require('child_process');
+
 const router = express.Router();
 router.post('/', async (req, res) => {
+  const jsonData = req.body
+  spawn('touch', ['holaaaa']/* , { uid: 1000 } */);
   const filename = moment().format('YYYY_MM_DD_hh_mm');
-  const p = join('/var/www/html/public/assets/webhook');
-  fs.writeFileSync(join(p, `${filename}.json`), JSON.stringify(req.body));
-  res.status(200).json({ hola: filename });
+  const fullfilename = join('/var/www/html/public/assets/webhook', `${filename}.json`);
+  fs.writeFileSync(fullfilename, JSON.stringify(jsonData));
+  res.status(200).json({ fileName: fullfilename });
   return undefined;
 });
-
 export default router;
