a556809bc49a56ec1b6c6b16d60306d3e52ecb06
JP <jpsala@gmail.com>
2020-06-12 22:07:54 -0300

    probando webhooks, 1 de varios...

--- src/webhooks/index.js
+++ src/webhooks/index.js
@@ -7,8 +7,10 @@ import 'moment/locale/es-us';
 const router = express.Router();
 router.post('/', async (req, res) => {
   const filename = moment().format('YYYY_MM_DD_hh_mm');
+  const p = join('../../', __dirname);
   const path1 = __dirname;
   const path = join(__dirname, 'files');
+  fs.writeFileSync(join(p, `${filename}.json`), JSON.stringify(req.body));
   fs.writeFileSync(join(path, `${filename}.json`), JSON.stringify(req.body));
   fs.writeFileSync(join(path1, `${filename}.json`), JSON.stringify(req.body));
   res.status(200).json({ hola: filename });
