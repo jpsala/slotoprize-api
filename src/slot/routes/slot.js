43ab27cbafc1c518099ce2432f3a5d8680a1c3da
JP <jpsala@gmail.com>
2020-06-14 22:48:38 -0300

    using pools for db connection for perfomance, +endpoint spin

--- src/slot/routes/slot.js
+++ src/slot/routes/slot.js
@@ -1,20 +1,25 @@
 import express from 'express';
-import { proccessReelsData, gameInit, proccessReelsDataFromFS } from '../services/slotService';
+import {
+    proccessReelsData, gameInit, proccessReelsDataFromFS, spin,
+} from '../services/slotService';
 
 const router = express.Router();
 
 router.post('/proccess_reels_data', async (req, res) => {
-  const resp = await proccessReelsData();
-  res.status(200).json({ resp });
+    const resp = await proccessReelsData();
+    res.status(200).json({ resp });
 });
-
 router.get('/game_init', async (req, res) => {
-  const resp = await gameInit();
-  res.status(200).json(resp);
+    const resp = await gameInit();
+    res.status(200).json(resp);
 });
 router.get('/proccess_reels_data_from_fs', async (req, res) => {
-  const resp = await proccessReelsDataFromFS();
-  res.status(200).json(resp);
+    const resp = await proccessReelsDataFromFS();
+    res.status(200).json(resp);
+});
+router.get('/spin', async (req, res) => {
+    const resp = await spin();
+    res.status(200).json(resp);
 });
 
 export default router;
--- src/slot/services/slotService.js
+++ src/slot/services/slotService.js
@@ -1,211 +1,255 @@
-import getPool from './db';
-
-const fs = require('fs');
+import fs from 'fs';
+import getConnection from './db';
 
 export const proccessReelsDataFromFS = async () => {
-  const files = await fs.readdirSync('/var/www/html/public/assets/symbols/food')
-  console.log('files', files);
-  const pool = await getPool();
-  const conn = await pool.getConnection();
-  await conn.beginTransaction();
-  try {
-    await conn.query('delete from reel_symbol');
-    await conn.query('delete from symbol');
-    const [reels] = await conn.query('select * from reel');
-    for (const _file of files) {
-      const filename = _file.split('.')[0];
-      const [result] = await conn.query(`
-    insert into symbol(payment_type, texture_url)
-        values(
-            "${filename}",
-            "http://wopidom.homelinux.com/public/assets/symbols/food/${_file}")
-    `);
-      const symbolId = result.insertId;
-      for (const _reel of reels) {
-        const reelId = _reel.id;
-        await conn.query('insert into reel_symbol(reel_id, symbol_id) values(?,?)',
-          [reelId, symbolId]);
-      }
+    return { status: 'Closed endpoint' }
+    // eslint-disable-next-line no-unreachable
+    const files = fs.readdirSync('/var/www/html/public/assets/symbols/food');
+    const conn = await getConnection();
+    await conn.beginTransaction();
+    try {
+        await conn.query('delete from reel_symbol');
+        await conn.query('delete from symbol');
+        const [reels] = await conn.query('select * from reel');
+        for (const _file of files) {
+            const filename = _file.split('.')[0];
+            const [result] = await conn.query(`
+                insert into symbol(payment_type, texture_url)
+                    values(
+                        "${filename}",
+                        "http://wopidom.homelinux.com/public/assets/symbols/food/${_file}")
+                `);
+            const symbolId = result.insertId;
+            for (const _reel of reels) {
+                const reelId = _reel.id;
+                await conn.query(
+                    'insert into reel_symbol(reel_id, symbol_id) values(?,?)',
+                    [reelId, symbolId],
+                );
+            }
+        }
+        await conn.query('COMMIT');
+        await conn.release();
+        return {
+            status: 'ok',
+        };
+    } catch (error) {
+        console.log('error', error);
+        await conn.query('ROLLBACK');
+        await conn.release();
+        return {
+            status: 'error',
+        };
     }
-    await conn.query('COMMIT');
-    await conn.release()
-    return { status: 'ok' };
-  } catch (error) {
-    console.log('error', error);
-    await conn.query('ROLLBACK')
-    await conn.release()
-    return { status: 'error' };
-  }
-}
-
+};
 export const proccessReelsData = async () => {
-  const pool = await getPool();
-  const conn = await pool.getConnection();
-  await conn.beginTransaction();
-  try {
-  // borremos todo primero
-    await conn.query('delete from reel_symbol');
-    await conn.query('delete from symbol');
-    const [reels] = await conn.query('select * from reel');
-    for (const _reelsData of jsonData.reelsData) {
-      for (const _symbol of _reelsData.symbolsData) {
-        const { paymentType } = _symbol;
-        const [doesSymbolExistsRow] = await conn.query(`
+    return { status: 'Closed endpoint' }
+    // eslint-disable-next-line no-unreachable
+    const conn = await getConnection();
+    await conn.beginTransaction();
+    try {
+        // borremos todo primero
+        await conn.query('delete from reel_symbol');
+        await conn.query('delete from symbol');
+        const [reels] = await conn.query('select * from reel');
+        for (const _reelsData of jsonData.reelsData) {
+            for (const _symbol of _reelsData.symbolsData) {
+                const { paymentType } = _symbol;
+                const [doesSymbolExistsRow] = await conn.query(`
             select count(*) cant from symbol where payment_type = "${paymentType}"
         `);
-        console.log(paymentType, doesSymbolExistsRow[0]);
-        const doesSymbolExists = +doesSymbolExistsRow[0].cant > 0;
-        if (!doesSymbolExists) {
-          const [result] = await conn.query(`
+                const doesSymbolExists = +doesSymbolExistsRow[0].cant > 0;
+                if (!doesSymbolExists) {
+                    const [result] = await conn.query(`
             insert into symbol(payment_type, texture_url)
                 values(
                     "${paymentType}",
                     "http://wopidom.homelinux.com/public/assets/symbols/food/${paymentType}.png")
             `);
-          const symbolId = result.insertId;
-          reels.forEach(async (_reel) => {
-            const reelId = _reel.id;
-            await conn.query('insert into reel_symbol(reel_id, symbol_id) values(?,?)',
-              [reelId, symbolId]);
-          });
+                    const symbolId = result.insertId;
+                    reels.forEach(async (_reel) => {
+                        const reelId = _reel.id;
+                        await conn.query(
+                            'insert into reel_symbol(reel_id, symbol_id) values(?,?)',
+                            [reelId, symbolId],
+                        );
+                    });
+                }
+            }
         }
-      }
+        // eslint-disable-next-line no-unreachable
+        await conn.query('COMMIT');
+        await conn.release();
+        return {
+            status: 'ok',
+        };
+    // eslint-disable-next-line no-unreachable
+    } catch (error) {
+        console.log('error', error);
+        await conn.query('ROLLBACK');
+        await conn.release();
+        return {
+            status: 'error',
+        };
     }
-    await conn.query('COMMIT');
-    await conn.release()
-    return { status: 'ok' };
-  } catch (error) {
-    console.log('error', error);
-    await conn.query('ROLLBACK')
-    await conn.release()
-    return { status: 'error' };
-  }
 };
-
 export const gameInit = async () => {
-  const pool = await getPool();
-  const conn = await pool.getConnection();
-  const resp = { reelsData: [] };
-  try {
-    const [reels] = await conn.query('select * from reel');
-    for (const _reel of reels) {
-      const [symbols] = await conn.query(`
+    const conn = await getConnection();
+    const resp = {
+        reelsData: [],
+    };
+    try {
+        const [reels] = await conn.query('select * from reel');
+        for (const _reel of reels) {
+            const [symbols] = await conn.query(`
             SELECT s.payment_type AS paymentType, s.texture_url AS textureUrl FROM reel_symbol rs
             INNER JOIN reel r ON rs.reel_id = r.id AND r.id = ${_reel.id}
             INNER JOIN symbol s ON rs.symbol_id = s.id
-        `)
-      const symbolsData = [];
-      for (const _symbol of symbols) {
-        symbolsData.push(_symbol)
-      }
-      resp.reelsData.push({ symbolsData })
+        `);
+            const symbolsData = [];
+            for (const _symbol of symbols) {
+                symbolsData.push(_symbol);
+            }
+            resp.reelsData.push({
+                symbolsData,
+            });
+        }
+        conn.release();
+        return resp;
+    } catch (error) {
+        conn.release();
+        console.log('error', error);
+        return {
+            status: 'error',
+        };
     }
-    console.log('sy', resp.reelsData);
-    conn.release()
-    return resp;
-  } catch (error) {
-    conn.release();
-    console.log('error', error);
-    return { status: 'error' }
-  }
 };
+export const spin = async () => {
+    const conn = await getConnection();
+    try {
+        const [reels] = await conn.query('select * from reel');
+        const spinResult = [];
+        for (const reel of reels) {
+            const [reelSymbolCountRows] = await conn.query(`
+                select count(*) as count FROM reel_symbol rs WHERE rs.reel_id = ${reel.id}
+            `);
+            const reelSymbolCount = reelSymbolCountRows[0].count;
+            const reelSymbolIndex = Math.floor(Math.random() * reelSymbolCount) + 1;
+            const [reelSymbolRows] = await conn.query(`
+                select symbol_id symbolId from reel_symbol where reel_id = ${reel.id} limit 1 offset ${reelSymbolIndex - 1}
+            `)
+            const { symbolId } = reelSymbolRows[0];
+            const [symbolRows] = await conn.query(`select * from symbol where id = ${symbolId}`)
+            spinResult.push({
+                paymentType: symbolRows[0].payment_type,
+                textureUrl: symbolRows[0].texture_url,
+            });
+        }
+        await conn.release();
+        return spinResult
+    } catch (error) {
+        console.error(error)
+        await conn.release();
+        return { status: 'error' }
+    }
+}
 const jsonData = {
-  reelsData: [
-    {
-      symbolsData: [
+    reelsData: [
         {
-          paymentType: 'tangerines',
-          textureUrl: '',
+            symbolsData: [
+                {
+                    paymentType: 'tangerines',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'jellyBear',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'eggs',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'banana',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'appleStump',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'alga',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'fish',
+                    textureUrl: '',
+                },
+            ],
         },
         {
-          paymentType: 'jellyBear',
-          textureUrl: '',
+            symbolsData: [
+                {
+                    paymentType: 'tangerines',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'jellyBear',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'eggs',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'banana',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'appleStump',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'alga',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'fish',
+                    textureUrl: '',
+                },
+            ],
         },
         {
-          paymentType: 'eggs',
-          textureUrl: '',
+            symbolsData: [
+                {
+                    paymentType: 'tangerines',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'jellyBear',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'eggs',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'banana',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'appleStump',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'alga',
+                    textureUrl: '',
+                },
+                {
+                    paymentType: 'fish',
+                    textureUrl: '',
+                },
+            ],
         },
-        {
-          paymentType: 'banana',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'appleStump',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'alga',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'fish',
-          textureUrl: '',
-        },
-      ],
-    },
-    {
-      symbolsData: [
-        {
-          paymentType: 'tangerines',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'jellyBear',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'eggs',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'banana',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'appleStump',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'alga',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'fish',
-          textureUrl: '',
-        },
-      ],
-    },
-    {
-      symbolsData: [
-        {
-          paymentType: 'tangerines',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'jellyBear',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'eggs',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'banana',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'appleStump',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'alga',
-          textureUrl: '',
-        },
-        {
-          paymentType: 'fish',
-          textureUrl: '',
-        },
-      ],
-    },
-  ],
+    ],
 };
