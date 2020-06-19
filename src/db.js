bf41adbace673f95a413e9619e98554cd0b32932
JP <jpsala@gmail.com>
2020-06-19 08:56:04 -0300

    fix various logs

--- src/db.js
+++ src/db.js
@@ -2,18 +2,18 @@ const mysql = require('mysql2/promise');
 
 let pool;
 module.exports = function db() {
-  if (pool) {
-    return pool;
-  }
-  const config = {
-    connectionLimit: 100,
-    host: 'localhost',
-    user: 'root',
-    password: 'lani0363',
-    database: 'iae-nuevo',
-    debug: false,
-    waitForConnections: true,
-    multipleStatements: true,
-  };
-  return mysql.createPool(config);
+    if (pool) {
+        return pool;
+    }
+    const config = {
+        connectionLimit: 100,
+        host: 'localhost',
+        user: 'root',
+        password: 'lani0363',
+        database: 'iae-nuevo',
+        debug: false,
+        waitForConnections: true,
+        multipleStatements: true,
+    };
+    return mysql.createPool(config);
 };
