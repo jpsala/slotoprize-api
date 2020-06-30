f4f3e765644ad35211ffb976bb12a5d770aeca3d
JP <jpsala@gmail.com>
2020-06-30 15:18:08 -0300

    -id's in country and it states

--- src/modules/meta/meta.repo/country.repo.ts
+++ src/modules/meta/meta.repo/country.repo.ts
@@ -4,16 +4,19 @@
 import {query} from '../meta.db'
 import {Country, State} from '../meta.types'
 
-export async function getCountries(fields: string[] | undefined = undefined): Promise<Country[]> {
+export async function getCountries(fields: string[] | undefined = undefined): Promise<Country[] | Partial<Country>> {
   const countries = await query(`
-    select * from  country
+    select id, name, phone_prefix from  country
   `, undefined, true, fields) as Country[]
   for (const country of countries) {
     const states = await query(`
-      select * from  state
+      select name from  state
       where country_id = ${country.id}
     `, undefined, true, fields) as State[]
-    country.states = states
+    delete (country as Partial<Country>).id
+    country.states = states.map((state) => {
+      return {name: state.name} as State
+    })
   }
   return countries
 }
