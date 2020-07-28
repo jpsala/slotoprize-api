8b06ab674fba0d85dff7ec4eeb1ab1145d37d824
JP <jpsala@gmail.com>
2020-07-28 19:14:38 -0300

    ...

--- src/modules/slot/slot.repo/skin.repo.ts
+++ src/modules/slot/slot.repo/skin.repo.ts
@@ -2,23 +2,57 @@
 import { query } from './../../../db'
 
 export interface Skin {
-  cameraColor: string;
-  headerColor: string;
-  bodyColor: string;
-  btnColor: string;
-  scrollBGColor: string;
-  scrollHandleColor: string;
-  darkTXTColor: string;
-  imgColor1: string;
-  imgColor2: string;
-  imgColor3: string;
-  spriteRenderColor: string;
   machineSkinTextureUrl: string;
+  colorPreset: {
+    cameraColor: string;
+    headerColor: string;
+    bodyColor: string;
+    btnColor: string;
+    scrollBGColor: string;
+    scrollHandleColor: string;
+    darkTXTColor: string;
+    imgColor1: string;
+    imgColor2: string;
+    imgColor3: string;
+    spriteRenderColor: string;
+  }
 }
-
+/*
+public Color cameraColor;
+        public Color headerColor;
+        public Color bodyColor;
+        public Color btnColor;
+        public Color scrollBGColor;
+        public Color scrollHandleColor;
+        public Color darkTXTColor;
+        public Color imgColor1;
+        public Color imgColor2;
+        public Color imgColor3;
+        public Color spriteRenderColor;
+*/
 export const getSkins = async (id?: number): Promise<Skin[]> => {
   const where = id ? ` id = ${id} ` : ' true '
-  const skins: Skin[] = await query('select * from skin where ' + where)
+  const rows = await query('select * from skin where ' + where)
+  const skins: Skin[] = []
+  for (const row of rows) {
+    const skin: Skin = {
+      machineSkinTextureUrl: row.machineSkinTextureUrl,
+      colorPreset: {
+        cameraColor: row.cameraColor,
+        headerColor: row.headerColor,
+        bodyColor: row.bodyColor,
+        btnColor: row.btnColor,
+        scrollBGColor: row.scrollBGColor,
+        scrollHandleColor: row.scrollHandleColor,
+        darkTXTColor: row.darkTXTColor,
+        imgColor1: row.imgColor1,
+        imgColor2: row.imgColor2,
+        imgColor3: row.imgColor3,
+        spriteRenderColor: row.spriteRenderColor
+      }
+    }
+    skins.push(skin)
+  }
   return skins
 }
 export const getSkin = async (id: number): Promise<Skin | undefined> => {
