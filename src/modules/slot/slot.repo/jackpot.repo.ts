cccb6eefdfbe2ea56b6854e4464adf961832f71e
JP <jpsala@gmail.com>
2020-09-21 19:15:34 -0300

    PrizeWinners now returns last 10 jackpots

--- src/modules/slot/slot.repo/jackpot.repo.ts
+++ src/modules/slot/slot.repo/jackpot.repo.ts
@@ -22,14 +22,19 @@ export type JackpotData = {
 export const getJackpotWinners = async (): Promise<PrizeWinners[]> =>
 {
   const data = await query(`
-    select concat(gu.first_name, ', ', gu.last_name) as winnerName,
-           jw.createdAt as date,
-          'https://assets.slotoprizes.tagadagames.com/img/jackpotPrize.png' as textureUrl,
-          'jackpot' as prizeName
-    from jackpot_win jw
-      inner join game_user gu on jw.game_user_id = gu.id
-      inner join jackpot j on jw.jackpot_id = j.id
-    where jw.createdAt and gu.first_name != '' and gu.last_name != ''
+        (select jw.id, concat(gu.first_name, ', ', gu.last_name)                         as winnerName,
+        jw.createdAt                                                      as date,
+        'https://assets.slotoprizes.tagadagames.com/img/jackpotPrize.png' as textureUrl,
+        'Jackpot'                                                         as prizeName
+      from jackpot_win jw
+          inner join game_user gu on jw.game_user_id = gu.id
+          inner join jackpot j on jw.jackpot_id = j.id
+      where jw.createdAt
+      and gu.first_name != ''
+      and gu.last_name != ''
+      order by jw.id desc
+      limit 10
+      ) order by 3 asc
   `)
   return data as PrizeWinners[]
 }
