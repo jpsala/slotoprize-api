ff87850a810555d30f6b5e9e402c60f76df7df1e
JP <jpsala@gmail.com>
2020-10-06 13:20:24 -0300

    winners for bo order desc

--- src/modules/meta/meta-services/winner.service.ts
+++ src/modules/meta/meta-services/winner.service.ts
@@ -4,34 +4,34 @@ import { getGameUserByDeviceId } from "../meta.repo/gameUser.repo"
 /* date, prize, origin, status, useId, user */
 export async function getWinnersForCrud(): Promise<any> {
   const select = `
-  select jw.id, gu.device_id,
-      if(last_name != '', concat(last_name, ', ', first_name), 'No name in Profile') as user,
-      if(gu.email != '', gu.email, 'No Email in Profile')          as email,
-      date_format(jw.createdAt, '%Y-%m-%d')                        as date,
-      'Jackpot'                                                    as origin,
-      jw.state                                                     as state,
-      j.prize                                                      as prize,
-      gu.id                                                        as userId
-   from jackpot_win jw
-      inner join jackpot j on jw.jackpot_id = j.id
-      inner join game_user gu on jw.game_user_id = gu.id
-   where jw.createdAT
-      union
-   select r.id, gu.device_id,
-      if(last_name != '', concat(last_name, ', ', first_name), '') as user,
-      if(gu.email != '', gu.email, 'n/a')                          as email,
-      date_format(rh.closing_date, '%Y-%m-%d')                     as date,
-      'Raffle'                                                     as origin,
-      r.state                                                      as state,
-      if(rl.id, concat(rl.name,' | ',rl.description), 'No localization data for this raffle')          as prize,
-      gu.id                                                        as userId
-    from raffle_wins rw
-      inner join raffle_history rh on rw.raffle_history_id = rh.id
-      inner join raffle r on rh.raffle_id = r.id
-      inner join game_user gu on r.winner = gu.id
-      left join raffle_localization rl on r.id = rl.raffle_id and rl.language_code = 'en-US'
-    where rh.closing_date
- order by 5
+    select jw.id, gu.device_id,
+        if(last_name != '', concat(last_name, ', ', first_name), 'No name in Profile') as user,
+        if(gu.email != '', gu.email, 'No Email in Profile')          as email,
+        date_format(jw.createdAt, '%Y-%m-%d')                        as date,
+        'Jackpot'                                                    as origin,
+        jw.state                                                     as state,
+        j.prize                                                      as prize,
+        gu.id                                                        as userId
+    from jackpot_win jw
+        inner join jackpot j on jw.jackpot_id = j.id
+        inner join game_user gu on jw.game_user_id = gu.id
+    where jw.createdAT
+        union
+    select r.id, gu.device_id,
+        if(last_name != '', concat(last_name, ', ', first_name), '') as user,
+        if(gu.email != '', gu.email, 'n/a')                          as email,
+        date_format(rh.closing_date, '%Y-%m-%d')                     as date,
+        'Raffle'                                                     as origin,
+        r.state                                                      as state,
+        if(rl.id, concat(rl.name,' | ',rl.description), 'No localization data for this raffle')          as prize,
+        gu.id                                                        as userId
+      from raffle_wins rw
+        inner join raffle_history rh on rw.raffle_history_id = rh.id
+        inner join raffle r on rh.raffle_id = r.id
+        inner join game_user gu on r.winner = gu.id
+        left join raffle_localization rl on r.id = rl.raffle_id and rl.language_code = 'en-US'
+      where rh.closing_date
+  order by 5 desc
 `
 const data = await query(select)
   for (const row of data) 
