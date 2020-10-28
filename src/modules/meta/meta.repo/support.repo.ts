31de740450b24a30a8551560663487003369fbec
JP <jpsala@gmail.com>
2020-10-28 15:45:51 -0300

    support messages in players BO now ordered from newer to older

--- src/modules/meta/meta.repo/support.repo.ts
+++ src/modules/meta/meta.repo/support.repo.ts
@@ -1,8 +1,8 @@
-import {BAD_REQUEST} from 'http-status-codes'
 import createError from 'http-errors'
-
-import { query, exec } from '../../../db'
+import { BAD_REQUEST } from 'http-status-codes'
+import { exec, query } from '../../../db'
 import { getSetting, setSetting } from './../../slot/slot.services/settings.service'
+
 
 export async function postSupportAdminForCrud(body: any): Promise<any>
 {
@@ -22,7 +22,7 @@ export async function supportAdminForCrud(): Promise<any>
 export async function getSupportRequestForCrud(userId: string): Promise<any>
 {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return await query(`select * from support_request where userId = ${userId}`)
+  return await query(`select * from support_request where userId = ${userId} order by id desc`)
 }
 export async function setSoporte(userId: number, body: any): Promise<any> {
   if(body?.email === "" || body?.message === "" || body?.name === "") throw createError(BAD_REQUEST, 'Missing or empty fields')
