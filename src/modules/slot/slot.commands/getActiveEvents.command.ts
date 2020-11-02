2f23280e01fdd6c739e532b6cbbc0eae70fad038
JP <jpsala@gmail.com>
2020-11-02 15:03:26 -0300

    get skin from db in getEventState command

--- src/modules/slot/slot.commands/getActiveEvents.command.ts
+++ src/modules/slot/slot.commands/getActiveEvents.command.ts
@@ -2,20 +2,24 @@ import PubSub from 'pubsub-js'
 import WebSocket from 'ws'
 import { getActiveEvents } from '../slot.services/events/events'
 import { EventPayload } from '../slot.services/events/event'
+import { getSkin } from '../slot.repo/skin.repo'
 import { WebSocketMessage, wsServer } from './../slot.services/webSocket/ws.service'
 
 type Message = { payload: any, client: WebSocket }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const runCommand = (cmd: string, data: Message): void => {
+export const runCommand = async (cmd: string, data: Message): Promise<void> => {
   const userIsDev = (data.client as any).isDev
 
   const activeEvents = getActiveEvents().filter(_event => {
-      if(_event.payload.devOnly && !userIsDev) return false
-      return true
+    if (_event.payload.devOnly && !userIsDev) return false
+    return true
   })
   const wsMessages: EventPayload[] = []
-  activeEvents.forEach(event => wsMessages.push(event.payload))
+  for (const event of activeEvents) {
+    if(event.payload.skinId) event.payload.skinData = await getSkin(event.payload.skinId)
+    wsMessages.push(event.payload)
+  }
   const wsMessage: Partial<WebSocketMessage> = {
     code: 200,
     message: 'OK',
@@ -23,6 +27,6 @@ export const runCommand = (cmd: string, data: Message): void => {
     payload: wsMessages
   }
   wsServer.send(wsMessage as WebSocketMessage, data.client)
-}
+  }
 
 PubSub.subscribe('getEventState', runCommand)
