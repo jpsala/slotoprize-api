6388c633977d4e5f7a2d6805a7ee915bc49f9654
JP <jpsala@gmail.com>
2020-11-14 00:39:09 -0300

    scripts to transfer logs to local, dont remove original files before coping them

--- bzip_dev_logs.sh
+++ bzip_dev_logs.sh
@@ -3,6 +3,6 @@ echo estoy en $(pwd)
 echo bzip2
 rm -f info.log.bz2
 rm -f error.log.bz2
-bzip2 info.log
-bzip2 error.log
+bzip2 -k info.log
+bzip2 -k error.log
 echo listo
--- trae_dev_logs.sh
+++ trae_dev_logs.sh
@@ -1,10 +1,10 @@
 ssh dev "sh /prg/api/bzip_dev_logs.sh"
 echo ################## volví del IAE
-rm info.log 2>/dev/null
-rm error.log 2>/dev/null
+rm info.dev.log 2>/dev/null
+rm error.dev.log 2>/dev/null
 echo scp
 scp -C  dev:/prg/api/info.log.bz2  .
 scp -C  dev:/prg/api/error.log.bz2  .
 echo bunzip2
-bunzip2 info.log.bz2
-bunzip2 error.log.bz2
+bunzip2 info.log.bz2 -d ./info.dev.log
+bunzip2 error.log.bz2 -d ./error.dev.log
