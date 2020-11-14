009731bde87ff25d892aeb4ec4fb8c33dbc47647
JP <jpsala@gmail.com>
2020-11-14 01:02:45 -0300

    remove comments from sh

--- trae_dev_logs.sh
+++ trae_dev_logs.sh
@@ -3,9 +3,9 @@ ssh dev "sh /prg/api/bzip_dev_logs.sh"
 mkdir -p logs.dev
 rm -rf logs.dev/* 2>/dev/null
 rm -rf logs.zip  .
-echo *************************
+echo "*************************"
 echo scp
-echo *************************
+echo "*************************"
 scp -C  dev:/prg/api/logs.zip  .
 echo unzip
 unzip logs.zip -d ./logs.dev
