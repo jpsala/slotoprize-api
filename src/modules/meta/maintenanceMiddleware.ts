50eaea5c41c9c728a1b42885b22490aa2882e896
JP <jpsala@gmail.com>
2020-11-16 23:08:53 -0300

    maintenanceMiddleware takes care of check if in maintenance mode for all endpoints

--- src/modules/meta/maintenanceMiddleware.ts
+++ src/modules/meta/maintenanceMiddleware.ts
@@ -6,11 +6,10 @@ import { verifyToken } from '../../services/jwtService'
 import { getSetting } from '../slot/slot.services/settings.service'
 import { getLocalization } from './meta-services/localization.service'
 import { getUserById } from "./meta-services/meta.service"
-import { getGameUser } from './meta.repo/gameUser.repo'
+import { getGameUser, getGameUserByDeviceId } from './meta.repo/gameUser.repo'
 import { GameUser, User } from './meta.types'
 
-export async function checkmaintenanceMode(req: Request, res: Response, next: NextFunction): Promise<any>
-{
+export async function checkmaintenanceMode(req: Request, res: Response, next: NextFunction): Promise<any> {
   const maintenanceMode = (await getSetting('maintenanceMode', '0')) === '1'
   const { 'dev-request': dev } = req.headers
   const isDev = (dev === 'true')
@@ -23,17 +22,21 @@ export async function checkmaintenanceMode(req: Request, res: Response, next: Ne
   
   const { decodedToken, error } = verifyToken(sessionToken as string)
 
+  let { deviceId } = req.query
+  if (!deviceId) deviceId = req.body?.deviceId
+  if (!deviceId) deviceId = req.params?.deviceId
+  const user = deviceId ? await getGameUserByDeviceId(deviceId as string) : undefined
   if ((error || !decodedToken.id) && maintenanceMode)
-    throw createHttpError(503, await getLocalization('maintenanceMode', undefined, 'We are in maintenance, we\'ll be back up soon!'))
+    throw createHttpError(503, await getLocalization('maintenanceMode', user?.id, 'We are in maintenance, we\'ll be back up soon!'))
   
   if (maintenanceMode) {
     let user: User | GameUser | undefined = await getGameUser(decodedToken.id)
     if (!user) {
       user = await getUserById(decodedToken.id)
-      if(user) user.isDev = true
+      if (user) user.isDev = true
     }
-    if(!user || (!isDev && !user.isDev))
-    throw createHttpError(503, await getLocalization('maintenanceMode', user.id, 'We are in maintenance, we\'ll be back up soon!'))
+    if (!user || (!isDev && !user.isDev))
+      throw createHttpError(503, await getLocalization('maintenanceMode', user.id, 'We are in maintenance, we\'ll be back up soon!'))
   }
   return next()
 }
