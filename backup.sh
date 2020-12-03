f455f934bc04e9649728a912aaef0c107e84b585
JP <jpsala@gmail.com>
2020-12-03 10:59:50 -0300

    backup.sh backups dev db in aws

--- backup.sh
+++ backup.sh
@@ -2,9 +2,10 @@ cd /prg/api
 
 now=$(date -d "today" +"%Y%m%d-%H%M")
 echo backup local
-/usr/bin/mysqldump wopidom --single-transaction -R -u root --password=lani0363 > wopidom-local_$now.sql
+# /usr/bin/mysqldump slotoprizes --single-transaction -R -u root --password=lani0363 > slotoprizes-local_$now.sql
+mysqldump slotoprizes --single-transaction -R -u admin --password=lani0363 -h slotoprizes.cdy8hosrrn6a.eu-west-3.rds.amazonaws.com > slotoprizes-local_$now.sql -vv
 echo bzip2
-/bin/bzip2 wopidom-local_$now.sql
+/bin/bzip2 slotoprizes-local_$now.sql
 echo listo el bzip2
-scp ./wopidom-local_$now.sql.bz2 dev.slotoprizes.tagadagames.com:/prg/api/backups
-echo backups ready in dev.slotoprizes.tagadagames.com:/prg/api/backups/wopidom-local_$now.sql.bz2
+scp ./slotoprizes-local_$now.sql.bz2 dev.slotoprizes.tagadagames.com:/prg/api/backups
+echo backups ready in dev.slotoprizes.tagadagames.com:/prg/api/backups/slotoprizes-local_$now.sql.bz2
