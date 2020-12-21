e6784332ad50b1348bce912470bbfe19b037125f
JP <jpsala@gmail.com>
2020-12-20 23:22:42 -0300

    small type

--- src/modules/slot/testtest/implemt.ts
+++ src/modules/slot/testtest/implemt.ts
@@ -4,5 +4,5 @@ import { getGameUserByDeviceId } from './../../meta/meta-services/meta.service'
 export const getUser = async (deviceId = 'fakeUserId'): Promise<GameUser> =>
 {
   const user = await getGameUserByDeviceId(deviceId)
-  return user
+  return user as GameUser
 }
