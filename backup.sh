2fc523e9c0e4d4ffbc2d8f181c9e95d840a2dbbd
JP <jpsala@gmail.com>
2020-12-23 15:43:28 -0300

    change backup to live db

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
 cd /prg/api
+
 now=$(date -d "today" +"%Y%m%d-%H%M")
 echo backup local
 # /usr/bin/mysqldump slotoprizes --single-transaction -R -u root --password=lani0363 > slotoprizes-local_$now.sql
-mysqldump slotoprizes --single-transaction -R -u admin --password=lani0363 -h slotoprizeslive.cdy8hosrrn6a.eu-west-3.rds.amazonaws.com > slotoprizes-local_$now.sql -vv
+mysqldump slotoprizes --single-transaction -R -u admin --password=lani0363 -h slotoprizes.cdy8hosrrn6a.eu-west-3.rds.amazonaws.com > slotoprizes-local_$now.sql -vv
 echo bzip2
 /bin/bzip2 slotoprizes-local_$now.sql
 echo listo el bzip2
