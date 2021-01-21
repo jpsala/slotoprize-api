b94e14c3107c557d3bbecd1f58048cea8877cf1b
JP <jpsala@gmail.com>
2021-01-21 10:10:41 -0300

    added google_callback

--- src/modules/portal/portal.routes.ts
+++ src/modules/portal/portal.routes.ts
@@ -8,4 +8,6 @@ router.route('/google_login').post(portalController.googleLoginPost)
 router.route('/facebook_login').post(portalController.facebookLoginPost)
 router.route('/sign_up').post(portalController.signUpPost)
 router.route('/activation').post(portalController.activationPost)
+router.route('/google_callback').post(portalController.googleCallback)
+router.route('/google_callback').get(portalController.googleCallback)
 export default router
